44b0336ad2e77f19cb595da5ccf5fa91d6169060
wickywinner <winner312.com@gmail.com>
2023-04-03 11:50:19 -0700

    modify setting.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9o^!5shqqavj@#@&36+y+nl3csjw2%hr3(wrzh@be05fd5ita)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
